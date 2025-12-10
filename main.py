b3a04c37c4f951cf236d9a90e7e6d70601e7455e
Amitabh Phule <amitabhphule4@gmail.com>
2025-12-11 00:07:19 +0530

    first program

---
+++ main.py
@@ -0,0 +1,9 @@
+import arihmatic as am
+print("hello world")
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Sum is:", am.sum(a, b))
+print("subtract is:", am.subtract(a, b))
+
+
